b1e5bcefb14a700a75b7cf2ba84f8621fe430059
Aidar Arzybekov Tashtanbekovich <aidararzybekovtashtanbekovich@MacBook-Air-Aidar.local>
2023-02-13 18:31:19 +0600

    first commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,34 @@
+import { Box } from '@chakra-ui/react'
+import type { NextPage, NextPageContext } from 'next'
+import { getSession, useSession } from 'next-auth/react'
+import Auth from '../components/Auth/Auth'
+import Chat from '../components/Chat/Chat'
+
+const Home: NextPage = () => {
+	const { data: session } = useSession()
+	console.log('here is data', session)
+
+	const reloadSession = () => {}
+
+	return (
+		<Box>
+			{session?.user?.username ? (
+				<Chat />
+			) : (
+				<Auth session={session} reloadSession={reloadSession} />
+			)}
+		</Box>
+	)
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+	const session = await getSession(context)
+
+	return {
+		props: {
+			session,
+		},
+	}
+}
+
+export default Home
